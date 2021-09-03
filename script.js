1662830b6118f524a11cf2541e7b11f2779111c1
andreacjp <89702911+andreacjp@users.noreply.github.com>
2021-09-02 20:40:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ var lorenIpsum = ["párrafo","texto","escrito","pasaje","enunciado"];
 function setup() {
     noCanvas();
     createElement("h1", elSaludo).parent(elEncabezado).id("title");
-    createA("index.html", "index").parent("vinculos");
-    createA("page.html", "page").parent("vinculos");
+    createA("index.html", "inicio").parent("vinculos");
+    createA("page.html", "página").parent("vinculos");
     if (dondeEstas) {
         portada();
     } else {
@@ -37,13 +37,13 @@ function setup() {
 }
 
 function portada() {
-    createSpan("Estás en index.html").parent("inicio");
+    createSpan("Estás en index.html").parent("title");
     createElement('p','Y este es un '+random(lorenIpsum)+' en la portada.').parent(loPrincipal);
     select("a:nth-child(1)").style("font-weight", "bold").style("color", "#000000");
 }
 
 function pagina() {
-    createSpan("Estás en page.html").parent("página");
+    createSpan("Estás en page.html").parent("title");
     createElement('p','Y este es un '+random(lorenIpsum)+' en la página.').parent(loPrincipal);
     select("a:nth-child(2)").style("font-weight", "bold").style("color", "#000000");
 }
